20c0804d3717fda14e9aa4bd9bbbab17428a0fe3
Arkadiusz <swiokloarkadiusz@gmail.com>
2020-03-17 11:22:05 +0100

    first part of homework done

---
+++ src/main/java/pl/juniorjavadeveloper/spring/crud/dao/entity/CarEntity.java
@@ -0,0 +1,4 @@
+package pl.juniorjavadeveloper.spring.crud.dao.entity;
+
+public class CarEntity {
+}
---
+++ src/main/java/pl/juniorjavadeveloper/spring/crud/service/mapper/CarMapper.java
@@ -0,0 +1,4 @@
+package pl.juniorjavadeveloper.spring.crud.service.mapper;
+
+public class CarMapper {
+}
